e9ef253943fc689ddc1de8de6a51a1ccfbbb2cd8
Manish Patel <mpatel27@bu.edu>
2021-04-15 23:08:42 -0400

    adding weather endpoint

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
-from flask import Flask
+from flask import request, Flask, jsonify
 from datetime import datetime
-from db import db, init
+# from db import db, init
+from weather import get_weather
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/weather', methods=['GET', 'POST'])
+def weather():
+    if request.method == 'GET':
+        lat, lon = request.args.get('lat'), request.args.get('long')
+        try:
+            forecast = get_weather(float(lat), float(lon))
+        except Exception as e:
+            forecast = [[e]]
+        #return f"Found lat: {lat} long: {lon}"
+        return jsonify(forecast)
+
 if __name__ == '__main__':
-    init()
+    # init()
     app.run()
---
+++ weather.py
@@ -0,0 +1,72 @@
+import http.client
+import json
+
+conn = http.client.HTTPSConnection("community-open-weather-map.p.rapidapi.com")
+
+headers = {
+    'x-rapidapi-key': WEATHER_API_KEY, 
+    'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com"
+    } 
+
+# requesting by city 
+# city = "boston"
+# req_path =  "/forecast/daily?q=" + city + "%2Cus&cnt=" + str(forecast_days) + "&units=imperial"
+
+
+# coordinates for boston, ma 
+#lat = 42.361145
+#lon = -71.057083
+
+def get_weather(lat:float, lon:float):
+
+    if not isinstance(lat, float):
+        lat = float(lat)
+    if not isinstance(lon, float):
+        lat = float(lon)
+
+    #assert(type(lat) == float)
+    #assert(type(lon) == float)
+
+    forecast_days = 17 
+    # 17 is the max into the future we can look at
+
+    req_path = "/forecast/daily?lat=" + str(lat) + "&lon=" + str(lon) + "&cnt=" + str(forecast_days) + "&units=imperial"
+
+    conn.request("GET", req_path, headers=headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    w_json = data.decode('utf-8')
+    # print(w_json)
+
+    data = json.loads(w_json)
+    #if 'cod' in data and data['cod'] == "400" or 'message' in data and data['message'] == "You have exceeded the rate limit per minute for your plan, BASIC, by the API provider":
+    #    return []
+    entries = json.dumps(data, indent=4)
+    # print(entries)
+
+    out = open("weather\wforecast.json", "w") 
+    json.dump(data, out, indent = 4) 
+    out.close() 
+
+    forecast = []
+    for i in range(forecast_days):
+        days = str(i+1) + " day(s) into the future: "
+        # print(data)
+        conditions = str(data["list"][i]["temp"]["day"]) + " degrees F and " + str(data["list"][i]["weather"][0]["description"])
+        forecast += [[days, conditions]]
+    # print(forecast[3])
+    res = forecast
+    
+    # weathertypes = ['rain and snow', 'snow' , 'light rain' , 'clear' , 'scattered clouds' , 'heavy rain', 'moderate rain', 'overcast clouds', 'broken clouds']
+    # return [{"temperature": item[1].split(' ')[0], "weather": [t for t in weathertypes if t in item[1]][0]} for item in res]
+    return [{"temperature": item[1].split(' ')[0], "weather": item[1][item[1].index('and') + 4:]} for item in res]
+
+# print(get_weather(47, -75))
+# for lat in range(-50, 50):
+#     for lon in range(-50, 50):
+#         try:
+#             get_weather(lat, lon)
+#         except Exception as e:
+#             print("bad lat", e, lat, lon)
