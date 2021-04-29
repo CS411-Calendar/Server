8d606d2098a9515bb3b25feb6240ce8c29fbcbf6
Curtis Mason <cumason@bu.edu>
2021-04-28 22:50:22 -0400

    can opt in for invites

--- app.py
+++ app.py
@@ -1,16 +1,62 @@
-from flask import request, jsonify
+from flask import request, jsonify, make_response
 from datetime import datetime
-# from db import db, init
+from db import Invite, User, InviteCalendar, Calendar, db, init
 from weather import get_weather
+from lib import createUserId
+from config import app
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def createErrorResponse(error: str):
+    return jsonify({
+        "error": error
+    })
+
+
+@app.route('/api/calendar/invite', methods=['POST'])
+def calendar():
+    userAgent = request.headers.get('User-Agent')
+    if request.method == 'POST' and userAgent:
+        userId = createUserId(request.remote_addr, userAgent)
+
+        if not User.query.get(userId):
+            user = User(id=userId)
+            db.session.add(user)
+        invite = Invite(ownerId=userId)
+        db.session.add(invite)
+        db.session.commit()
+        return make_response(invite.json(), 201)
+
+
+@app.route('/api/calendar/invite/<int:inviteId>', methods=['GET', 'PUT'])
+def inviteLink(inviteId: int):
+    if request.method == 'GET':
+        data = Invite.query.get(inviteId)
+        if data:
+            return make_response(data.json(), 200)
+        else:
+            return make_response(createErrorResponse("Not Found"), 404)
+    elif request.method == 'PUT':
+        invite = Invite.query.get(inviteId)
+        if not invite:
+            return make_response(createErrorResponse("Invite ID Not Found"), 404)
+        calendarId = request.form.get('id')
+        if not calendarId:
+            return make_response(createErrorResponse("Missing Public Calendar ID"), 400)
+        calendar = Calendar.query.get(calendarId)
+        if not calendar:
+            calendar = Calendar(id=calendarId)
+            db.session.add(calendar)
+        invite_calendar = InviteCalendar.query.get([inviteId, calendarId])
+        if not invite_calendar:
+            invite_calendar = InviteCalendar(inviteId=inviteId, calendarId=calendarId)
+            db.session.add(invite_calendar)
+        
+        db.session.commit()
+        return make_response(invite_calendar.json(), 201)
+
 
 @app.route('/weather', methods=['GET', 'POST'])
 def weather():
-
     if request.method == 'GET':
         lat, lon = request.args.get('lat'), request.args.get('long')
         try:
@@ -19,6 +65,7 @@ def weather():
             return 'Internal Server Error', 500
         return jsonify(forecast), 200
 
+
 if __name__ == '__main__':
-    # init()
+    init()
     app.run()
--- db/__init__.py
+++ db/__init__.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask import jsonify
 from config import app
 db = SQLAlchemy(app)
 
@@ -6,6 +7,11 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.String(75), primary_key=True)
     
+    def json(self):
+        return jsonify({
+            "id": self.id
+        })
+
     def __repr__(self):
         return '<User %r>' % self.id
 
@@ -14,6 +20,13 @@ class Invite(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     ownerId = db.Column(db.String(75), db.ForeignKey('user.id'), nullable=False)
     owner = db.relationship('User')
+
+    def json(self):
+        return jsonify({
+            "id": self.id,
+            "ownerId": self.ownerId
+        })
+
     def __repr__(self):
         return '<Invite %r>' % self.id
 
@@ -28,17 +41,28 @@ class InviteCalendar(db.Model):
             inviteId, calendarId,
         ),
     )
+
+    def json(self):
+        return jsonify({
+            "inviteId": self.inviteId,
+            "calendarId": self.calendarId
+        })
+
     def __repr__(self):
         return '<InviteCalendar %r>' % self.id
 
 
 class Calendar(db.Model):
     id = db.Column(db.String(320), primary_key=True)
+
+    def json(self):
+        return jsonify({
+            "id": self.id
+        })
+        
     def __repr__(self):
         return '<Calendar %r>' % self.id
 
 def init():
     db.create_all()
-    # admin = User()
-    # db.session.add(admin)
     db.session.commit()
